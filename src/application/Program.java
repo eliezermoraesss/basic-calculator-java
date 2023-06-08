313615284b293eea262d13fca37f71c05c115a6e
eliezer <eliezer.moraes@outlook.com>
2023-06-08 15:41:42 -0300

    fix exceptions

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,16 +22,16 @@ public class Program {
 			System.out.print("Digite + para somar, - para subtrair, * para multiplicar e / para dividir: ");
 			op = scan.next();
 			
-			for(String str : operadores) {
-				if(str.equals(op)) {
-					throw new InputMismatchException();
-				}
-			}
+			if(op.equals("+") || op.equals("-")|| op.equals("*") || op.equals("/")) {
 					
 			System.out.print("Digite o valor 1: ");
 			value1 = scan.nextFloat();
 			System.out.print("Digite o valor 2: ");
 			value2 = scan.nextFloat();
+			
+			} else {
+				throw new InputMismatchException();
+			}
 
 			switch (op) {
 			case "+":
@@ -53,6 +53,7 @@ public class Program {
 			System.out.println();
 			} catch (InputMismatchException e) {
 				System.out.println("Digite um caracter válido -> + - * /");
+				System.out.println();
 			}
 		}
 	}
